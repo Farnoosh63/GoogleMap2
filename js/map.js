67d484ddf57201ab32bad76e2b110126a7ded21f
Farnoosh Johnson and Kevin Deganos <kevin@>
2016-05-26 10:11:02 -0700

    enabled placing markers on map with mouse click

---
+++ js/map.js
@@ -0,0 +1,184 @@
+var map;
+
+exports.initMap = function() {
+  geocoder = new google.maps.Geocoder();
+  var origin_place_id = null;
+  var destination_place_id = null;
+  var travel_mode = google.maps.TravelMode.WALKING;
+  map = new google.maps.Map(document.getElementById('map'), {
+    // mapTypeControl: false,
+    center: {lat: 45.55252525134013, lng: -122.71728515625},
+    // scrollwheel: false,
+    zoom: 13
+  });
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsDisplay.setMap(map);
+
+  var origin_input = document.getElementById('origin-input');
+  var destination_input = document.getElementById('destination-input');
+  var modes = document.getElementById('mode-selector');
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(origin_input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination_input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(modes);
+
+  var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
+  origin_autocomplete.bindTo('bounds', map);
+  var destination_autocomplete =
+  new google.maps.places.Autocomplete(destination_input);
+  destination_autocomplete.bindTo('bounds', map);
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  function setupClickListener(id, mode) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener('click', function() {
+      travel_mode = mode;
+      route(origin_place_id, destination_place_id, travel_mode,
+        directionsService, directionsDisplay);
+      });
+    }
+    setupClickListener('changemode-walking', google.maps.TravelMode.WALKING);
+    setupClickListener('changemode-transit', google.maps.TravelMode.TRANSIT);
+    setupClickListener('changemode-driving', google.maps.TravelMode.DRIVING);
+
+    function expandViewportToFitPlace(map, place) {
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+    }
+
+    origin_autocomplete.addListener('place_changed', function() {
+      var place = origin_autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+    expandViewportToFitPlace(map, place);
+
+    // If the place has a geometry, store its place ID and route if we have
+    // the other place ID
+    origin_place_id = place.place_id;
+    route(origin_place_id, destination_place_id, travel_mode,
+      directionsService, directionsDisplay);
+    });
+
+    destination_autocomplete.addListener('place_changed', function() {
+      var place = destination_autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+    expandViewportToFitPlace(map, place);
+
+    // If the place has a geometry, store its place ID and route if we have
+    // the other place ID
+    destination_place_id = place.place_id;
+    route(origin_place_id, destination_place_id, travel_mode,
+      directionsService, directionsDisplay);
+    });
+
+    function route(origin_place_id, destination_place_id, travel_mode,
+      directionsService, directionsDisplay) {
+        if (!origin_place_id || !destination_place_id) {
+          return;
+        }
+      directionsService.route({
+        origin: {'placeId': origin_place_id},
+        destination: {'placeId': destination_place_id},
+        travelMode: travel_mode
+      }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
+    //traffic layer
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+    //transit layer
+    var transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map);
+
+
+    panorama = map.getStreetView();
+
+    // This event listener calls addMarker() when the map is clicked.
+    google.maps.event.addListener(map, 'click', function(event) {
+      addMarker(event.latLng, map);
+    });
+
+    // Adds a marker to the map.
+    function addMarker(location, map) {
+      // Add the marker at the clicked location, and add the next-available label
+      // from the array of alphabetical characters.
+      var marker = new google.maps.Marker({
+        position: location,
+        label: labels[labelIndex++ % labels.length],
+        map: map
+      });
+      markers.push(marker);
+    }
+    // Sets the map on all markers in the array.
+    function setMapOnAll(map) {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+      }
+    }
+
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+      setMapOnAll(null);
+    }
+
+    // Deletes all markers in the array by removing references to them.
+    var deleteMarkers = function() {
+      clearMarkers();
+      markers = [];
+    }
+
+};
+
+
+
+var labels= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex =0;
+var markers = [];
+var longLatOutput;
+var panorama;
+var geocoder;
+
+exports.toggleStreetView =function () {
+  var toggle = panorama.getVisible();
+  if (toggle === false) {
+    panorama.setVisible(true);
+  } else {
+    panorama.setVisible(false);
+  }
+
+  var address = document.getElementById('origin-input').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      // var marker = new google.maps.Marker({
+      //   map: map,
+      //   position: results[0].geometry.location
+      // });
+      longLatOutput = results[0].geometry.location;
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+    panorama.setPosition(longLatOutput);
+    panorama.setPov(/** @type {google.maps.StreetViewPov} */({
+      heading: 265,
+      pitch: 0
+    }));
+  });
+
+};
