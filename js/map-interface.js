69782c8254d4f97e016223bb93ba91923f06c370
Farnoosh Johnson and Kevin Deganos <kevin@>
2016-05-26 10:22:42 -0700

    enabled placing markers on map with mouse click

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,8 +1,9 @@
 var initMap = require("./../js/map.js").initMap;
 var toggleStreetView = require("./../js/map.js").toggleStreetView;
+
 $(document).ready(function() {
+
   $(window).load(initMap);
-  // var newMap = new initMap();
   $("#toggleView").click(toggleStreetView);
   // $("#clearMarkers").click(clearMarkers);
   $("#deleteMarkers").click(deleteMarkers);
